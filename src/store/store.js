4241a5026be9bbc4840674447fbf82583a30a10a
Mike <axis139@gmail.com>
2018-02-27 17:40:41 -0800

    updated to include dist for push to gh-pages with git

--- src/store/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import reducer from "./reducers";
-
-let store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
-export default store;
